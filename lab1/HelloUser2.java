4fccf35eddbccd3cb6addb5b954fa15f542691d6
Mythili Manasa Karra <mmkarra@unix2.lt.ucsc.edu>
2019-12-15 15:14:46 -0800

    Added lab1

---
+++ lab1/HelloUser2.java
@@ -0,0 +1,9 @@
+//Mythili Karra
+//mmkarra
+//12M
+//01/22/2019
+//This class simply prints a Hello statement
+class HelloUser2 {
+public static void main(String[] args) {
+System.out.println("Hello, this is HelloUser2!");
+}}
